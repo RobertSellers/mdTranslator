8c0285bc5d4e210930f5d7bbaf72b47e5dd83c33
Robert Sellers <robert.sellers@usda.gov>
2023-07-03 15:21:00 -0400

    hotefix

--- lib/adiwg/mdtranslator/writers/iso19115_3/classes/class_miMetadata.rb
+++ lib/adiwg/mdtranslator/writers/iso19115_3/classes/class_miMetadata.rb
@@ -79,7 +79,7 @@ module ADIWG
                   codelistClass = MD_Codelist.new(@xml, @hResponseObj)
                   # partyClass = CI_ResponsibleParty.new(@xml, @hResponseObj)
                   # hierarchyClass = Hierarchy.new(@xml, @hResponseObj)
-                  localeClass = PT_Locale.new(@xml, @hResponseObj)
+                  # localeClass = PT_Locale.new(@xml, @hResponseObj)
                   # representationClass = SpatialRepresentation.new(@xml, @hResponseObj)
                   # systemClass = MD_ReferenceSystem.new(@xml, @hResponseObj)
                   # extensionClass = MD_MetadataExtensionInformation.new(@xml, @hResponseObj)
@@ -144,19 +144,22 @@ module ADIWG
                              'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
                              'xsi:schemaLocation' => "http://standards.iso.org/iso/19115/-3/mdb/2.0 #{remoteSchema}"}) do
 
-
+                     # metadata information - file identifier (default: UUID)
+                     s = hMetaInfo[:metadataIdentifier][:identifier]
 
                      # metadata information - metadata character ('utf-8')
-                     @xml.tag!('mdb:defaultLocale') do
-                        codelistClass.writeXML('lan', 'UTF-8')
-                        end
-   
-                        # metadata information - metadata character ('utf-8')
-                        @xml.tag!('lan:characterSet') do
-                           codelistClass.writeXML('lan', 'iso_characterSet', 'UTF-8')
-                        end
-                     
+                     @xml.tag!('lan:characterEncoding') do
+                        codelistClass.writeXML('lan', 'iso_characterSet', 'UTF-8')
+                     end
+                     # metadata information - metadata language ('eng; USA')
+                     s = hLocale[:languageCode]
+                     @xml.tag!('lan:languageCode') do
+                        codelistClass.writeXML('lan', 'iso_language', s)
+                     end
 
+                     # metadata information - parent identifier
+                     s = nil
+                     hParent = hMetaInfo[:parentMetadata]
 
 
                   end # gmi:MI_Metadata tag
